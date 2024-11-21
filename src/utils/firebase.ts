6e64448c53ba5a27cea1c250a4642616f070ce43
anindyadiva06 <anindyadiva99@gmail.com>
2024-11-21 22:11:24 +0700

    Initial commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,19 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6y2qmefNOIQ4lRksYCCb5l_aZgO5zAbM",
+    authDomain: "labmobile-pertemuan10.firebaseapp.com",
+    projectId: "labmobile-pertemuan10",
+    storageBucket: "labmobile-pertemuan10.firebasestorage.app",
+    messagingSenderId: "867119393848",
+    appId: "1:867119393848:web:ad6f61a5be438aec21cacd",
+    measurementId: "G-0NL1TFR59C"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
